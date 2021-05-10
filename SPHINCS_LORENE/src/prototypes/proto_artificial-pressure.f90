65825cd2f4f4ca6a9a818ac04f2ba3278d2508db
Francesco Torsello <44674676+francescotorsello@users.noreply.github.com>
2021-05-10 11:39:02 +0200

    FT 10.05.2021: Following the APM procedure as outlined in setup_TOV_star.
    Former-commit-id: 4f706750994cafcaf51f19b4696d15a598f94f63
    Former-commit-id: ecba5a9f1eb7431bbb21aaee808f67751d6479af [formerly 76cd0b7a851a4d50269252b952c56a7f1bf0eced]
    Former-commit-id: 354f10741ded8c557658190e80aca46b88c223c7

--- SPHINCS_LORENE/src/prototypes/proto_artificial-pressure.f90
+++ SPHINCS_LORENE/src/prototypes/proto_artificial-pressure.f90
@@ -22,17 +22,21 @@ PROGRAM proto_apm
                                  escap,t,n1,n2,pos_u,h,nu,npart,&
                                  npm,Nstar,allocate_sph_memory
   USE options,             ONLY: basename,ikernel,ndes,av_max,icow
+  USE input_output,        ONLY: read_options
+  USE units,               ONLY: umass,m0c2_CU,set_units
 
   USE APM,                 ONLY: density_loop,position_correction,&
                                  assign_h
   USE set_h,               ONLY: exact_nei_tree_update
+  USE analyze,             ONLY: COM
 
   IMPLICIT NONE
 
   INTEGER, PARAMETER:: unit_id  = 23
   INTEGER, PARAMETER:: max_npart= 5D+6
-  INTEGER:: tmp, ios, itr, a, nout
+  INTEGER:: npart_tmp, tmp, ios, itr, a, nout
 
+  DOUBLE PRECISION com_x,com_y,com_z,com_d
   DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE:: pos
   DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE:: lapse_parts
   DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE:: shift_parts_x
@@ -53,6 +57,8 @@ PROGRAM proto_apm
   DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE:: sph_density
   DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE:: nstar0
   DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE:: h0
+
+  DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE:: Nstar_real
 
   LOGICAL:: exist
 
@@ -92,7 +98,7 @@ PROGRAM proto_apm
   ALLOCATE( nstar0(max_npart) )
   ALLOCATE( h0(max_npart) )
 
-  finalnamefile= "lorene-bns-id-particles.dat"
+  finalnamefile= "lorene-bns-id-particles-shells.dat"
 
   INQUIRE( FILE= TRIM(finalnamefile), EXIST= exist )
 
@@ -111,7 +117,7 @@ PROGRAM proto_apm
   ENDIF
 
   PRINT *, " * Reading file " // TRIM(finalnamefile) // "..."
-  npart= 0
+  npart_tmp= 0
   ! Skipping the first 4 header rows
   READ( unit_id, * )
   READ( unit_id, * )
@@ -156,41 +162,57 @@ PROGRAM proto_apm
       PRINT *
       EXIT
     ENDIF
-    npart= npart + 1
+    npart_tmp= npart_tmp + 1
   ENDDO
+  IF( tmp /= npart_tmp )THEN
+    PRINT *, "** ERROR! Mismatch in the values of the particle number read", &
+             " from " // TRIM(finalnamefile), "in two ways. "
+    PRINT *, "tmp= ", tmp, ", npart_tmp= ", npart_tmp
+    PRINT *, "Stopping..."
+    STOP
+  ENDIF
 
   CLOSE( unit_id )
 
-  pos                  = pos                  ( :, 1:npart )
-  lapse_parts          = lapse_parts          ( 1:npart )
-  shift_parts_x        = shift_parts_x        ( 1:npart )
-  shift_parts_y        = shift_parts_y        ( 1:npart )
-  shift_parts_z        = shift_parts_z        ( 1:npart )
-  baryon_density_parts = baryon_density_parts ( 1:npart )
-  energy_density_parts = energy_density_parts ( 1:npart )
-  specific_energy_parts= specific_energy_parts( 1:npart )
-  pressure_parts       = pressure_parts       ( 1:npart )
-  v_euler_parts_x      = v_euler_parts_x      ( 1:npart )
-  v_euler_parts_y      = v_euler_parts_y      ( 1:npart )
-  v_euler_parts_z      = v_euler_parts_z      ( 1:npart )
-  v                    = v                    ( :, 1:npart )
-  nu0                  = nu0                  ( 1:npart )
-  nlrf0                = nlrf0                ( 1:npart )
-  Ye0                  = Ye0                  ( 1:npart )
-  Theta0               = Theta0               ( 1:npart )
-  sph_density          = sph_density          ( 1:npart )
-  nstar0               = nstar0               ( 1:npart )
-  h0                   = h0                   ( 1:npart )
+  pos                  = pos                  ( :, 1:npart_tmp )
+  lapse_parts          = lapse_parts          ( 1:npart_tmp )
+  shift_parts_x        = shift_parts_x        ( 1:npart_tmp )
+  shift_parts_y        = shift_parts_y        ( 1:npart_tmp )
+  shift_parts_z        = shift_parts_z        ( 1:npart_tmp )
+  baryon_density_parts = baryon_density_parts ( 1:npart_tmp )
+  energy_density_parts = energy_density_parts ( 1:npart_tmp )
+  specific_energy_parts= specific_energy_parts( 1:npart_tmp )
+  pressure_parts       = pressure_parts       ( 1:npart_tmp )
+  v_euler_parts_x      = v_euler_parts_x      ( 1:npart_tmp )
+  v_euler_parts_y      = v_euler_parts_y      ( 1:npart_tmp )
+  v_euler_parts_z      = v_euler_parts_z      ( 1:npart_tmp )
+  v                    = v                    ( :, 1:npart_tmp )
+  nu0                  = nu0                  ( 1:npart_tmp )
+  nlrf0                = nlrf0                ( 1:npart_tmp )
+  Ye0                  = Ye0                  ( 1:npart_tmp )
+  Theta0               = Theta0               ( 1:npart_tmp )
+  sph_density          = sph_density          ( 1:npart_tmp )
+  nstar0               = nstar0               ( 1:npart_tmp )
+  h0                   = h0                   ( 1:npart_tmp )
 
   !-------------------------------!
   !-- Needed calls from SPHINCS --!
   !-------------------------------!
 
+  ! setup unit system
+  CALL set_units('NSM')
+
+  !-------------------------------!
+  !-- read main options for run --!
+  !-------------------------------!
+  CALL read_options
+
+  npart= npart_tmp
   CALL allocate_SPH_memory
 
-  pos_u                = pos                  ( :, 1:npart )
-  vel_u                = v                    ( :, 1:npart )
-  h                    = h0                   ( 1:npart )
+  pos_u                = pos                  ( :, 1:npart_tmp )
+  vel_u                = v                    ( :, 1:npart_tmp )
+  h                    = h0                   ( 1:npart_tmp )
 
   CALL allocate_RCB_tree_memory_3D(npart)
   iorig(1:npart)= (/ (a,a=1,npart) /)
@@ -211,7 +233,43 @@ PROGRAM proto_apm
   ! setup_uniform_sphere first place positions in a cubic lattice or a
   ! close-packed lattice, then assigns the smoothing length
 
-  CALL assign_h(npart,nout,pos_u,h0,h)
+  PRINT *, "npart_tmp=", npart_tmp
+  PRINT *
+  CALL assign_h(npart_tmp,npart,pos_u,h0,h)
+
+  !STOP
+
+  ! measure SPH-particle number density
+  ALLOCATE( Nstar_real(max_npart) )
+  nu= 1.0D0
+  CALL density_loop(npart,pos_u,nu,h,Nstar)
+
+  ! In setup_uniform_sphere, get_profile_density is called
+  ! this computed nstar, but we have it from the ID file
+
+  ! assign nu's
+  nu= nstar/Nstar
+
+  ! In setup_uniform_sphere, reset_COM is called
+  !CALL reset_COM(npart,nu,pos_u)
+  CALL COM(npart,pos_u,nu,com_x,com_y,com_z,com_d)
+  DO a=1,npart
+     pos_u(1,a)= pos_u(1,a) - com_x
+     pos_u(2,a)= pos_u(2,a) - com_y
+     pos_u(3,a)= pos_u(3,a) - com_z
+  ENDDO
+
+  ! at this point, the particle should be mirrored (?)
+  ! TODO: distinguish the two stars and write the prototype to handle one star
+  !       at the time
+
+  ! Re-estimate nu
+  CALL density_loop(npart,pos_u,nu,h,Nstar)
+
+  ! Here you should get the LORENE ensity on the new positions...this means you
+  ! need to compile LORENE and SPHINCS_LORENE...however, you might be able to
+  ! use the SUBROUTINES bound to LORENE without creating a bns object
+
 
   PRINT *, "** End of PROGRAM proto_apm."
   PRINT *
